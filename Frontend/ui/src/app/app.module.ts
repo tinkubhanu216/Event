2f0d0c969e349c595e270e5951f531b9f56dbf11
sandeep <divakaruni4@gmail.com>
2020-07-07 11:06:22 +0530

    [sandeep]:basic ui

--- Frontend/ui/src/app/app.module.ts
+++ Frontend/ui/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'flatpickr/dist/flatpickr.css';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import {ButtonModule} from 'primeng/button';
 
 import { MessageService } from 'primeng/api';
 
@@ -19,12 +26,21 @@ import { PlannerSidebarComponent } from './planner/planner-sidebar/planner-sideb
     PlannerSidebarComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    FlatpickrModule.forRoot(),
+    NgbModalModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    ButtonModule,
+    
   ],
   providers: [MessageService],
   bootstrap: [AppComponent]
